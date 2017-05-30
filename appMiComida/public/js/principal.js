685281cca2ea0fcb2923d77b6aa3deb5eade0707
Jaussman Sanchez <Jaussman Sanchez>
2017-05-30 10:27:27 -0500

    vista index

---
+++ appMiComida/public/js/principal.js
@@ -0,0 +1,7 @@
+$(".button-collapse").sideNav();
+$('.modal').modal();
+$('select').material_select();
+$('.datepicker').pickadate({
+      selectMonths: true, // Creates a dropdown to control month
+      selectYears: 200 // Creates a dropdown of 15 years to control year
+});
